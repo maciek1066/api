3ec53729d1f0ffa4c3e177beea5952d201056f2a
maciek1066 <maciek1066@gmail.com>
2018-04-24 21:01:39 +0200

    test api

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import viewsets
+from quickstart.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    # API endpoint
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    # API endpoint
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
